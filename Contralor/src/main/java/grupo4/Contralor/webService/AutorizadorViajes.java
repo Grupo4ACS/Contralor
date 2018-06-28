1f1bd1205552a356fef9d54371ab5804e02c2ebd
Gabriel Agustín VÉNTOLA <tuto_inxs@ca.frre.utn.edu.ar>
2018-06-28 20:34:12 -0300

    listo

---
+++ Contralor/src/main/java/grupo4/Contralor/webService/AutorizadorViajes.java
@@ -0,0 +1,26 @@
+package grupo4.Contralor.webService;
+
+import java.util.Random;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("AutorizarViaje")
+
+@Produces(MediaType.APPLICATION_JSON)
+public class AutorizadorViajes {
+  
+    private Random rand = new Random();
+    
+    @GET
+    public Response get() {
+    	if (rand.nextBoolean()) {
+    		return Response.accepted().build();
+    	} else {
+    		return Response.notAcceptable(null).build();
+    	}    	
+    }
+}
---
+++ Contralor/src/main/java/grupo4/Contralor/webService/ServiceConfig.java
@@ -0,0 +1,9 @@
+package grupo4.Contralor.webService;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("servicios")
+public class ServiceConfig extends Application {
+
+}
